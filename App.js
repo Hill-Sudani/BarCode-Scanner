a3472e52edf7e0eea9d454d327e928bf090e47fd
Hill-Sudani <71578540+Hill-Sudani@users.noreply.github.com>
2021-04-14 15:48:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default class App extends React.Component {
+  
+}
---
+++ Screens/ScanScreen.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default class ScannerClass extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hascamerapermission: null,
+      scanned: false,
+      scannedData: '',
+      buttonState: 'normal',
+    };
+  }
+
+  getCameraPermissions = async (id) => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      /*status === "granted" is true when user has granted permission
+          status === "granted" is false when user has not granted the permission
+        */
+      hasCameraPermissions: status === 'granted',
+      buttonState: id,
+      scanned: false,
+    });
+  };
+
+  handleBarCodeScanned = async ({ type, data }) => {
+    if (this.state.buttonState === 'clicked') {
+      this.setState({
+        scanned: true,
+        scannedData: data,
+        buttonState: 'normal',
+      });
+    }
+  };
+
+  render() {
+    return (
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            this.setState({
+              buttonState: 'normal',
+            });
+          }}>
+          <Text>Click Me</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress = {this.getCameraPermissions} title = 'QR CODE SCANNER'><Text>Scan QR Code</Text></TouchableOpacity>
+      </View>
+
+      if (buttonState !== 'normal' && hasCameraPermissions) {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
+    }
+    );
+  }
+}
