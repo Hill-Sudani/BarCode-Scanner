38f18dd5b239a019291cb69fb7e26716b60fc9ef
Hill-Sudani <71578540+Hill-Sudani@users.noreply.github.com>
2021-05-27 15:45:35 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
+import ScanScreen from './Screens/ScanScreen';
 
 export default class App extends React.Component {
-  
+  <ScanScreen />
 }
